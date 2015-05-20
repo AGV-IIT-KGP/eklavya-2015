954e8eedb7097df68a492c2d72aabd2fdc101247
Anurag Bharadwaj <chatrasen@gmail.com>
2015-05-20 16:30:46 +0530

    Made changes in the waypoint selector so that it publishes the PoseStamped target to the high level planner in the odom frame. There is no need of waypoint_navigator from now on

--- navigation/waypoint_selector/src/main.cpp
+++ navigation/waypoint_selector/src/main.cpp
@@ -3,29 +3,42 @@
 int main(int argc, char* argv[]) {
     std::string node_name = "waypoint_selector"; //launchfile-> <node .... name="waypoint_selector" />
     ros::init(argc, argv, node_name);
-    ros::NodeHandle node_handle;
 
     int strategy;
     std::string file_name;
-    node_handle.getParam(std::string("/") + node_name + std::string("/filename"), file_name);
-    node_handle.getParam(std::string("/") + node_name + std::string("/strategy"), strategy);
+    ros::NodeHandle node_handle1;
+    node_handle1.getParam(std::string("/") + node_name + std::string("/filename"), file_name);
+    node_handle1.getParam(std::string("/") + node_name + std::string("/strategy"), strategy);
+
     WaypointSelector waypoint_selector(file_name, strategy);
-
-    ros::Publisher next_waypoint_publisher = node_handle.advertise<sensor_msgs::NavSatFix>("waypoint_selector/next_waypoint", buffer_size);
-    ros::Publisher nml_flag_publisher = node_handle.advertise<std_msgs::Bool>("waypoint_selector/nml_flag", buffer_size);
-    ros::Subscriber planner_status_subscriber = node_handle.subscribe("local_planner/status", buffer_size, &WaypointSelector::set_planner_status, &waypoint_selector);
-    ros::Subscriber fix_subscriber = node_handle.subscribe("vn_ins/fix", buffer_size, &WaypointSelector::set_current_position, &waypoint_selector);
-
     std_msgs::Bool nml_flag;
     ros::Rate loop_rate(loop_rate_hz);
-    while (ros::ok()) {
-        node_handle.getParam(std::string("/") + node_name + std::string("/proximity"), waypoint_selector.proximity_);
 
-        next_waypoint_publisher.publish(waypoint_selector.findTarget());
-        nml_flag.data = waypoint_selector.isInsideNoMansLand();
-        nml_flag_publisher.publish(nml_flag);
+    geometry_msgs::PoseStamped proposed_target;
+    geometry_msgs::Pose2D pose2D_target;
 
+    int flag = 0; //to make sure convert_gps_to_odom is executed only once
+    while (ros::ok()) 
+    {
         ros::spinOnce();
+
+        if(waypoint_selector.subscription_started_gps && waypoint_selector.subscription_started_odom && flag==0)
+        {
+            flag = 1;
+            waypoint_selector.convert_gps_to_odom();
+        }
+        
+        node_handle1.getParam(std::string("/") + node_name + std::string("/proximity"), waypoint_selector.proximity_);
+        
+        if(waypoint_selector.subscription_started_gps && waypoint_selector.subscription_started_odom)
+        {
+            pose2D_target =waypoint_selector.findTarget();
+            proposed_target =waypoint_selector.convert_Pose2D_to_PoseStamped(pose2D_target);
+            waypoint_selector.next_waypoint_publisher.publish(proposed_target);
+            nml_flag.data = waypoint_selector.isInsideNoMansLand();
+            waypoint_selector.nml_flag_publisher.publish(nml_flag);
+        }
+        
         loop_rate.sleep();
     }
 }
