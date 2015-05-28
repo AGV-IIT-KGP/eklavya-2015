215aa710e435c5ff5ca97563ff9d95b5bba66033
Aranya <aranya.dan1996@gmail.com>
2015-05-28 18:49:45 +0530

    added bash files and instructions to use lane_detector

---
+++ perception/step2.sh
@@ -0,0 +1,5 @@
+cd ~/catkin_ws/src/eklavya-2015/perception/lane_detector/data/
+rosrun lane_detector svmTrain Training_file_name #Enter the name of file of output from generate samples in 
+													#place of "Training_file_name"
+cd ../launch/
+gedit module.launch
