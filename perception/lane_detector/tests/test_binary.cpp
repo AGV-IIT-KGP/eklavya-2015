5bdb9bb3cc56172f1ce39c3aa74661d6303966aa
todivasudha <todivasudha@gmail.com>
2015-09-11 16:58:27 +0530

    Included the function applyThreshold in laneDetector.hpp

--- perception/lane_detector/tests/test_binary.cpp
+++ perception/lane_detector/tests/test_binary.cpp
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "../src/detector/getLaneBinary.cpp"
+#include <laneDetector.hpp> 
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
